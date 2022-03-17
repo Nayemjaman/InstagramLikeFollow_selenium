26effbc8b07b335c127bbab45fc4b9cd98398511
Nayem Jaman Tusher <njaman26@gmail.com>
2022-03-17 14:55:59 +0600

    function based log in

--- bots.py
+++ bots.py
@@ -9,41 +9,43 @@ import time
 from up import *
 
 
-#create webdriver object
+# #create webdriver object
 driver = webdriver.Chrome('C:/Users/USER/Desktop/insta/InstagramLikeFollow_selenium/chromedriver.exe')
-driver.get('https://www.instagram.com')
-
-driver.close()
 
 
-username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
-password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
-
-
-# enter username and password
-username.send_keys(uname)
-password.send_keys(passw)
-
-button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
-alert = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/div/div/div/button'))).click()
-alert2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div/div/div/div[3]/button[2]'))).click()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# time.sleep(50)
 # driver.close()
+
+
+
+def login(driver,uname,passw):
+    driver.get('https://www.instagram.com')
+    username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+    password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+    username.send_keys(uname)
+    password.send_keys(passw)
+    button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+    try:
+        alert = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/div/div/div/button'))).click()
+    except:
+        pass
+    try:
+        alert2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div/div/div/div[3]/button[2]'))).click()
+    except:
+        pass
+
+login(driver,uname,passw)
+
+
+
+
+
+
+
+
+
+
+# # time.sleep(50)
+# # driver.close()
 
 
 
